12e68383f00f0c4322740b743830c6f8329b644a
lgyer <lgy330395508@gmail.com>
2015-08-25 11:37:52 +0800

    Create ci framework with hmvc and template.

---
+++ site/front/company/controllers/company.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Company extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('company');
+	}
+
+}
+
+/* End of file company.php */
+/* Location: ./application/front/company/controllers/company.php */
